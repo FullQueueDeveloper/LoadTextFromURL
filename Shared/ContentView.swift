5c3940c336275cc3d0e0d56b7021cbfd99e0b93c
Full Queue Developer <FullQueueDeveloper@fullqueuedeveloper.com>
2022-06-22 15:28:40 -0400

    viewer question

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+  @State var text: String = "Loading..."
+  var body: some View {
+    Text(text)
+      .padding()
+      .onAppear() {
+        loadTextFile()
+      }
+  }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+  func loadTextFile() {
+    Task {
+      let (data, _) = try await URLSession.shared.data(for: URLRequest(url: URL(string: "https://www.tomorrow.io/weather-api/")!))
+      let string = String(data: data, encoding: .utf8)
+      self.text = string ?? "error"
     }
+  }
 }
