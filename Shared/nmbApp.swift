87e1a9182df9039f22d10f31fc75b300e487b4f3
Full Queue Developer <FullQueueDeveloper@fullqueuedeveloper.com>
2022-06-22 15:18:58 -0400

    Initial Commit

---
+++ Shared/nmbApp.swift
@@ -0,0 +1,17 @@
+//
+//  nmbApp.swift
+//  Shared
+//
+//  Created by Full Queue Developer on 6/22/22.
+//
+
+import SwiftUI
+
+@main
+struct nmbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
